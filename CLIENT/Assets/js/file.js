abacee6aa1868e6ee08c18ed28dbb514b570f98d
niongre <dieudonneniongre.6771@gmail.com>
2024-11-05 23:51:42 +0000

    Update of home and training pages

--- CLIENT/Assets/js/file.js
+++ CLIENT/Assets/js/file.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded",function(){
     fetch("/CLIENT/Src/Public/Components/header.html")
         .then(response => response.text())
         .then(data => {
@@ -15,27 +15,6 @@ document.addEventListener("DOMContentLoaded", function(){
             document.getElementById('footer').innerHTML = data;
         });
 });
-
-function voirDétails(event){
-    var details = document.getElementById('details');
-    if (event === 'python'){
-        details.innerHTML = `
-            <h2>Atelier de Python</h2>
-            <p>Introduction à la programmation en Python pour les débutants.</p>
-            <p>Date : 20/10/2024</p>
-            <p>Lieu : Salle 101, Bâtiment A</p>
-        `;
-    }
-    else if (event === 'cybersecurity'){
-        details.innerHTML = `
-            <h2>Conférence sur la cybersécurité</h2>
-            <p>Les dernières tendances en matière de cybersécurité.</p>
-            <p>Date : 27/10/2024</p>
-            <p>Lieu : Amphithéâtre 2, Bâtiment B</p>
-        `;
-    }
-    // Ajoutez d'autres conditions pour d'autres événements
-}
 
 // inscription
 document.getElementById('signupForm').addEventListener('submit', function(event) {
@@ -73,4 +52,18 @@ document.querySelectorAll('#nav-tab>[data-bs-toggle="tab"]').forEach(el => {
       const scrollElem = document.querySelector(`${target} [data-bs-spy="scroll"]`);
       bootstrap.ScrollSpy.getOrCreateInstance(scrollElem).refresh();
     });
-  });
+});
+
+//navbar
+$(document).ready(function(){ 
+    $(".navbar-toggler").click(function(){ 
+        if($("#navbarNav").hasClass("active")){ 
+            $("#navbarNav").removeClass("active"); 
+            $("#container").removeClass("hidden"); 
+        } 
+        else{ 
+            $("#navbarNav").addClass("active"); 
+            $("#container").addClass("hidden"); 
+        } 
+    }); 
+});
